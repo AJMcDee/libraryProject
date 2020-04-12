723d5810044473ebf0df74b4e44862eab4111e93
Anna McDougall <anna.mcdougall@gmail.com>
2020-04-12 19:26:37 +0200

    Added a delete function to each entry

--- script.js
+++ script.js
@@ -15,8 +15,20 @@ addBookSubmit.addEventListener("click", e => {
     document.getElementById("addBookFormDiv").style.visibility = "hidden";
 })
 
+function createRemoveButton(divID) {
+    const currentDiv = document.getElementById(divID)
+    let removeButton = document.createElement('button')
+    removeButton.textContent = "Remove Book"
+    removeButton.id =  `removeBook${divID}`
+    currentDiv.insertAdjacentElement("beforeend", removeButton)
+    removeButton.addEventListener("click", e => {
+        currentDiv.parentNode.removeChild(currentDiv);
+        myLibrary[divID] = null;
+    })
+}
+
+
 function Book(title, author, pages, read) {
-    this.ID = Date.now();
     this.title = title;
     this.author = author;
     this.pages = pages;
@@ -37,25 +49,35 @@ function addNewBook() {
     const pages = document.getElementById("bookPageCount").value
     const read = document.getElementById("bookRead").checked
     const newBook = new Book(title, author, pages, read)
-    addBook(newBook);
-    const index = myLibrary.length - 1;
+    const index = addBook(newBook);
     insertInDom(newBook, index)
+    createRemoveButton(index)
 }
 
 function addBook(bookObj) {
-        if (bookObj.read === true) {
-            bookObj.readString = "Completed"
-        } else if (bookObj.read === false) {      
-            bookObj.readString = "Unread";
-        } else {
-            bookObj.readString = "Invalid entry";
-        }
-    myLibrary.push(bookObj);
+    if (bookObj.read === true) {
+        bookObj.readString = "Completed"
+    } else if (bookObj.read === false) {      
+        bookObj.readString = "Unread";
+    } else {
+        bookObj.readString = "Invalid entry";
+    }
+
+    // Add new book to empty slot if prior book deleted
+    const index = myLibrary.indexOf(null);
+    if (index === -1) {
+        myLibrary.push(bookObj)
+        return myLibrary.length - 1
+    } else {
+        myLibrary[index] = bookObj;
+        return index
+    }  
 }
 
 function render(){
     myLibrary.forEach(function(item, index) {
         insertInDom(item, index)
+        createRemoveButton(index)
     }); 
 }
 
